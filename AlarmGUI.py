216ba88dff8f47d895f0c49cfd0e0e746da84d80
Andrew Mitchell <andrew4.mitchell@northumbria.ac.uk>
2013-03-22 22:26:23 +0000

    number functions added to GUI

--- AlarmGUI.py
+++ AlarmGUI.py
@@ -6,6 +6,34 @@ import tkinter.messagebox
 
 AlarmCode = "1234"
 
+def one():
+	code_entry.insert(END,"1")
+def two():
+	code_entry.insert(END,"2")
+def three():
+	code_entry.insert(END,"3")
+def four():
+	code_entry.insert(END,"4")
+def five():
+	code_entry.insert(END,"5")
+def six():
+	code_entry.insert(END,"6")
+def seven():
+	code_entry.insert(END,"7")
+def eight():
+	code_entry.insert(END,"8")
+def nine():
+	code_entry.insert(END,"9")
+def zero():
+	code_entry.insert(END,"0")
+def clear():
+	code_entry.delete(0, END)
+def delete():
+	if len(code_entry == 1):
+		code_entry.delete(1)
+	elif len(code_entry == 2):
+		code_entry.delete(2)
+
 def getCode():
 	global userEntered
 	userEntered = (code_entry.get())
@@ -28,26 +56,27 @@ code_entered = IntVar()
 
 #Add Entry field for  code data, calls class Constrained Entry for validation
 code_entry= ConstrainedEntry(mainframe, width=7, textvariable=code_entered)
+code_entry.delete(0, END)
 code_entry.grid(columnspan = 2, column=2, row=5, sticky=(W, E))
 
 #Add buttons to frame using grid layout
-ttk.Button(mainframe, text="1", command =one).grid(column=1, row=1, sticky=NW)
-ttk.Button(mainframe, text="2").grid(column=2, row=1, sticky=NW)
-ttk.Button(mainframe, text="3").grid(column=3, row=1, sticky=NW)
-ttk.Button(mainframe, text="4").grid(column=1, row=2, sticky=NW)
-ttk.Button(mainframe, text="5").grid(column=2, row=2, sticky=NW)
-ttk.Button(mainframe, text="6").grid(column=3, row=2, sticky=NW)
-ttk.Button(mainframe, text="7").grid(column=1, row=3, sticky=NW)
-ttk.Button(mainframe, text="8").grid(column=2, row=3, sticky=NW)
-ttk.Button(mainframe, text="9").grid(column=3, row=3, sticky=NW)
-ttk.Button(mainframe, text="Clear").grid(column=1, row=4, sticky=NW)
-ttk.Button(mainframe, text="0").grid(column=2, row=4, sticky=NW)
-ttk.Button(mainframe, text="Delete").grid(column=3, row=4, sticky=NW)
+ttk.Button(mainframe, text="1", command=one).grid(column=1, row=1, sticky=NW)
+ttk.Button(mainframe, text="2", command=two).grid(column=2, row=1, sticky=NW)
+ttk.Button(mainframe, text="3", command=three).grid(column=3, row=1, sticky=NW)
+ttk.Button(mainframe, text="4", command=four).grid(column=1, row=2, sticky=NW)
+ttk.Button(mainframe, text="5", command=five).grid(column=2, row=2, sticky=NW)
+ttk.Button(mainframe, text="6", command=six).grid(column=3, row=2, sticky=NW)
+ttk.Button(mainframe, text="7", command=seven).grid(column=1, row=3, sticky=NW)
+ttk.Button(mainframe, text="8", command=eight).grid(column=2, row=3, sticky=NW)
+ttk.Button(mainframe, text="9", command=nine).grid(column=3, row=3, sticky=NW)
+ttk.Button(mainframe, text="Clear", command = clear).grid(column=1, row=4, sticky=NW)
+ttk.Button(mainframe, text="0", command=zero).grid(column=2, row=4, sticky=NW)
+ttk.Button(mainframe, text="Delete", command=delete).grid(column=3, row=4, sticky=NW)
 
-ttk.Button(mainframe, text="OK", command =getCode).grid(column=5, row=5, sticky=SE)
-ttk.Button(mainframe, text="Cancel").grid(column=6, row=5, sticky=SE)
+ttk.Button(mainframe, text="Disable Alarm", command =getCode).grid(column=5, row=4, sticky=E)
+ttk.Button(mainframe, text="Enable Alarm").grid(column=5, row=5, sticky=SE)
 
-ttk.Label(mainframe, text="Enter Code:").grid(column=1, row=5, sticky=W)
+ttk.Label(mainframe, text="Alarm Code:").grid(column=1, row=5, sticky=W)
 
 
 
